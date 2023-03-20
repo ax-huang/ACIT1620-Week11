a433f34f329a56e4fd38d4b0188fd1f4810e7b11
Andrew <ahuang84@my.bcit.ca>
2023-03-20 10:18:12 -0700

    calcArea Fucntion w/ return statement

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,11 +20,23 @@
 // greet('andrew')
 
 //-----Function Declaration with Two Args and Deafualt Value-----
-function greet(name, time = 'day'){
-    console.log(`good ${time} ${name}`);
+// function greet(name, time = 'day'){
+//     console.log(`good ${time} ${name}`);
+// }
+
+//###Arrow Fucntion###
+// // let greet = (user, time = 'day') => console.log(`Good ${time} ${user}`)
+
+// greet('andrew');
+
+//-----Calc Area of Cicrcle Function w/ Return Statement-----
+let radius 
+
+do{
+    radius = Number(prompt("Enter a radius"));
 }
+while(isNaN(radius))
 
-//Arrow Fucntion
-// let greet = (user, time = 'day') => console.log(`Good ${time} ${user}`)
+let calcArea = radius => (Math.pow(radius,2) * Math.PI).toFixed(2);
 
-greet('andrew');
+alert(`The area of a circle with radius ${radius} is ${calcArea(radius)}`);
