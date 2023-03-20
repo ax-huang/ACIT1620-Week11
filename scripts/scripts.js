66de07df1d1ef810a00e14d74981e29b8686268e
Andrew <ahuang84@my.bcit.ca>
2023-03-20 09:25:18 -0700

    Function Expression

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,13 @@
-function greet(name){
-    console.log(`hello ${name}`);
+//-----Function Declaration-----
+// function greet(name){
+//     console.log(`hello ${name}`);
+// }
+
+// greet('andrew');
+
+//-----Function Expression-----
+let greet = function(name){
+    console.log(`Hello ${name}`)
 }
 
 greet('andrew');
